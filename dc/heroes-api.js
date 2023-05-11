415b1b50913b9a819e95b49e813b8e844e25b6cb
Lam Thai Thanh Long <39092725+lamthaithanhlong@users.noreply.github.com>
2023-05-11 11:05:24 -0500

    Update heroes-api.js

--- dc/heroes-api.js
+++ dc/heroes-api.js
@@ -1,5 +1,6 @@
 exports.gerDcHeroes = (req, res) => {
   const heroes = require('./heroes.json');
+  // comment
   let heroesMatch = heroes.filter((hero)=>{
       if(hero.publisher == "DC Comics"){
           return hero;
